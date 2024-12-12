a791945f17bdf650baf5a2a20eb90688979aa3a4
Androiddev126 <98218535+Androiddev126@users.noreply.github.com>
2024-12-13 02:01:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $p=$_post['phone'];
 
 $con=mysqli_connect("sql12.freesqldatabase.com","sql12751437","Ghiuf9eTM9","sql12751437");
 $sql="INSERT INTO MEMBERS(Name,Voter_Id,ward,taluk,village,district,phone) values('$n','$i','$w','$t','$v','$d','$p')";
-$r=$mysqli_query($con,$sql);	
+$r=mysqli_query($con,$sql);	
 	if ($r)
 	{    echo "MEMBERS ADDED SUCCESS";
 	}
