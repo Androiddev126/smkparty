4de57e5b6c568f96c1c6aaf15e89fd9ff6e9d681
Androiddev126 <98218535+Androiddev126@users.noreply.github.com>
2024-12-13 01:29:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$n=$_post['name'];
+$i=$_post['voterid'];
+$w=$_post['ward'];
+$t=$_post['taluk'];
+$v=$_post['village'];
+$d=$_post['district'];
+$p=$_post['phone'];
+
+$con=mysqli_connect("sql12.freesqldatabase.com","sql12751437","Ghiuf9eTM9","sql12751437");
+$sql="INSERT INTO MEMBERS(Name,Voter_Id,ward,taluk,village,district,phone) values('$n','$i','$w','$t','$v','$d','$p')";
+$r=$mysqli_query($con,$sql);	
+	if ($r)
+	{    echo "MEMBERS ADDED SUCCESS";
+	}
+	else
+	{    echo " MEMBERS NOT ADDED";
+	}	
+	
+?>
